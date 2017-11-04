92c6e4f329a404e1f2a43e356c655f04d0768902
zsx <790125989@qq.com>
2017-11-04 23:32:30 +0800

    提交文件

---
+++ buy_transaction.php
@@ -0,0 +1,7 @@
+<?php
+	
+	include './Common/bootstrop.php';
+	$url = 'http://'.DOMAIN_NAME.'/index.php?app=app&c=seckill&a=addQsec&gid=2&type=transaction';
+	$result = file_get_contents($url);
+	
+	var_dump($result);
